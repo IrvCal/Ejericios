eab0aa8b8cbd440e4f7fe78f7ee13a7e3c59a575
irvcal <juanirvincalderon@gmail.com>
2022-02-10 22:04:08 -0600

    Se agrego un metodo para hacer un agrupamiento de cursos por categoria

--- FunctionalProgramingExcercices/src/functional_programing/E1.java
+++ FunctionalProgramingExcercices/src/functional_programing/E1.java
@@ -1,8 +1,9 @@
 package functional_programing;
 
+import functional_programing.model.Course;
+
 import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Collectors;
 
 /**
  1. Cuente el número de letras minúsculas en una cadena (pista: vea el método de caracteres de los objetos de cadena).
@@ -88,63 +89,5 @@ public class E1 {
 
     public static int palabraNumLetras(Course course){
         return course.getName().length();
-    }
-}
-class Course {
-    private String name;
-    private String category;
-    private int reviewScore;
-    private int numberOfStudents;
-
-    public Course() {
-    }
-
-    public Course(String name, String category, int reviewScore, int numberOfStudents) {
-        this.name = name;
-        this.category = category;
-        this.reviewScore = reviewScore;
-        this.numberOfStudents = numberOfStudents;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
-
-    public int getReviewScore() {
-        return reviewScore;
-    }
-
-    public void setReviewScore(int reviewScore) {
-        this.reviewScore = reviewScore;
-    }
-
-    public int getNumberOfStudents() {
-        return numberOfStudents;
-    }
-
-    public void setNumberOfStudents(int numberOfStudents) {
-        this.numberOfStudents = numberOfStudents;
-    }
-
-    @Override
-    public String toString() {
-        return "Course{" +
-                "name='" + name + '\'' +
-                ", category='" + category + '\'' +
-                ", reviewScore=" + reviewScore +
-                ", numberOfStudents=" + numberOfStudents +
-                '}';
     }
 }
